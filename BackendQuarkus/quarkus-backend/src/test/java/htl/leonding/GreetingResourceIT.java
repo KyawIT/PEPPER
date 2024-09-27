2e4802b5d293396d56c8e86686021e53e4192b9f
KyawIT <kyawhtethtun65@gmail.com>
2024-09-27 19:41:23 +0200

    Added Initial Routes to Quarkus Backend

--- BackendQuarkus/quarkus-backend/src/test/java/htl/leonding/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package htl.leonding;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
